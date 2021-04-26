3cb3de64daa903366fefc839cfbd4a28f2b94f89
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-04-26 16:22:10 +0500

    Publication Date works correctly

---
+++ db/20210413101544_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.date :publication_year
+      t.string :publication_place
+      t.string :publishing_company
+      t.integer :number_of_copies
+    end
+  end
+end
