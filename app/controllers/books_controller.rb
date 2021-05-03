d43bab1d6af1003a2dd7f41647ddd791fe742230
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-05-03 14:44:02 +0500

    create method added to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,5 +7,23 @@ class BooksController < ApplicationController
   def new
 
   end
+
+  def create
+    @book = Book.new(product_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book,
+          notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created,
+        location: @book }
+      else
+        puts @book.errors.full_messages
+        format.html { render :new }
+        format.json { render json: @book.errors,
+          status: :unprocessable_entity }
+      end
+    end
+  end
   
 end
