5eae162f0734a03099db62426f8eadd1e8e4051d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-05-08 11:33:29 +0500

    name hstore added to countris migration

--- db/migrate/20210418104350_create_countries.rb
+++ db/migrate/20210418104350_create_countries.rb
@@ -1,8 +1,9 @@
 class CreateCountries < ActiveRecord::Migration[6.0]
   def change
     create_table :countries do |t|
-
+      t.hstore :name_translations
       t.timestamps
+      
     end
   end
 end
