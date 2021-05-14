15a2b77d19077510e2c6e0c06bbf8d2def393e5c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-05-14 13:35:05 +0500

    reference to book_title_check script added to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("packs/book_title_check")
 require jquery
 require jquery_ujs
 
