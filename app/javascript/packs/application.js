cd6d0f89cb65585c0929b21071ac76189da7b051
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-05-19 13:35:41 +0500

    yard add jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("jquery")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("packs/book_title_check")
-
-//= require jquery
-//= require jquery_ujs
-
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
