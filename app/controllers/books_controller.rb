a9cf921e19615493f0830b0c95afc34ea1263009
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-05-28 13:05:49 +0500

    more experiments with ajax in new books view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,11 +51,13 @@ class BooksController < ApplicationController
   end
 
   def check_if_book_title_exists
+    @book = Book.new(book_params)
+    @title = @book[params[:title]]
     @similar_books = Book.where("title LIKE ?", "%#{params[:title]}%")
     if @similar_books.present?
         render json: {:success => 0, :message => 'Similar books exist', :book_available => true}
       else  
-        render json: {:success => 1, :message => 'No similiar books', :book_available => false}
+        render json: {:success => 1, :message => @title, :book_available => false}
     end   
   end
   
