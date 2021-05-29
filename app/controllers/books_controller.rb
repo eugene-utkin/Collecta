57d5ce39d306c2d3bc0216eb2e58fcf054c59fbb
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-05-29 09:09:49 +0500

    more attempts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,7 +51,7 @@ class BooksController < ApplicationController
   end
 
   def check_if_book_title_exists
-    @book = params[:title]
+    @book = Book.new(book_params)
     @similar_books = Book.where("title LIKE ?", "%#{params[:title]}%")
     if @similar_books.present?
         render json: {:success => 0, :message => 'Similar books exist', :book_available => true}
