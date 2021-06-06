d725b0eda7c882f4e4c75e400c95742a09f76d06
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-06-06 12:23:22 +0500

    similar_books_content added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :countries
 
   get '/check_if_book_title_exists' => 'books#check_if_book_title_exists', as: :check_if_book_title_exists
+  get '/similar_books_content' => 'similar_books_content', as: :similar_books_content
 end
