fcb5f07c200c559abc7c8749875987fcd05f54c1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-06-08 20:11:12 +0500

    trying to figure out, why empty field produces result

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -53,9 +53,9 @@ class BooksController < ApplicationController
   def check_if_book_title_exists
     @book = params[:title]
     @similar_books = Book.where("title LIKE ?", "%#{params[:title]}%")
-    if @similar_books.present?
-        render json: {:success => 0, :message => @similar_books, :book_available => true}
-      else  
+    if @similar_books.length > 0
+        render json: {:success => 0, :message => @similar_books.map, :book_available => true}
+    else  
         render json: {:success => 1, :message => 'The book is not in database', :book_available => false}
     end   
   end
