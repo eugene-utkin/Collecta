897fcb34ffad780f9d37f0bd894bc2605c3625ef
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-07-11 11:17:48 +0500

    add class script added to check_if.js.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,7 +57,6 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @similar_books.present? && @book.length > 0
         format.js
-        format.json { render json: {:success => 0, :message => @similar_books, :book_available => true} }
       else  
         format.json { render json: {:success => 1, :message => 'The book is not in database', :book_available => false} }
       end
