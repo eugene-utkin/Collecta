efc79521bf264fef654bd57bbb0ab8952a3e7d03
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-08-21 15:17:58 +0500

    create authors migration has birth_year

--- db/migrate/20210818081529_create_authors.rb
+++ db/migrate/20210818081529_create_authors.rb
@@ -2,6 +2,7 @@ class CreateAuthors < ActiveRecord::Migration[6.0]
   def change
     create_table :authors do |t|
       t.hstore :name_translations
+      t.integer :birth_year
       t.timestamps
     end
   end
