4b8637db56847a14416ec1e479b31cb366c5963b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-08-22 12:38:49 +0500

    create authors migration has bio_translations

--- db/migrate/20210818081529_create_authors.rb
+++ db/migrate/20210818081529_create_authors.rb
@@ -2,6 +2,7 @@ class CreateAuthors < ActiveRecord::Migration[6.0]
   def change
     create_table :authors do |t|
       t.hstore :name_translations
+      t.hstore :bio_translations
       t.integer :birth_year
       t.integer :birth_month
       t.integer :birth_day
