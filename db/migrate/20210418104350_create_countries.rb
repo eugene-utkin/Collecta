27db5945080e528ef7307d222ba47c384b71a462
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-08-22 12:47:27 +0500

    create countries migration has flag_image

--- db/migrate/20210418104350_create_countries.rb
+++ db/migrate/20210418104350_create_countries.rb
@@ -2,6 +2,7 @@ class CreateCountries < ActiveRecord::Migration[6.0]
   def change
     create_table :countries do |t|
       t.hstore :name_translations
+      t.string :flag_image
       t.timestamps
     end
   end
