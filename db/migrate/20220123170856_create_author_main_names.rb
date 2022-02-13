22493179cddcb9b939fd6dda4a4a8b776e3dce43
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-13 18:10:48 +0500

    author name migration changed

--- db/migrate/20220123170856_create_author_main_names.rb
+++
@@ -1,8 +0,0 @@
-class CreateAuthorMainNames < ActiveRecord::Migration[6.0]
-  def change
-    create_table :author_main_names do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end
---
+++ db/migrate/20220123170856_create_author_names.rb
@@ -0,0 +1,8 @@
+class CreateAuthorNames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :author_names do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
