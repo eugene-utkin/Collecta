f8e9a75e54fb79b07e6cd39e49dc53328281d5ac
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-13 18:11:26 +0500

    status added to authors name

--- db/migrate/20220123170856_create_author_names.rb
+++ db/migrate/20220123170856_create_author_names.rb
@@ -2,6 +2,7 @@ class CreateAuthorNames < ActiveRecord::Migration[6.0]
   def change
     create_table :author_names do |t|
       t.string :name
+      t.string :status
       t.timestamps
     end
   end
