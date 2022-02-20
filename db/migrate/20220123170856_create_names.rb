be3f3531c5839d6d7020dcc3a0b27e219f4d28e5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-20 19:00:50 +0500

    names migration altered

--- db/migrate/20220123170856_create_names.rb
+++ db/migrate/20220123170856_create_names.rb
@@ -2,6 +2,8 @@ class CreateNames < ActiveRecord::Migration[6.0]
   def change
     create_table :names do |t|
       t.string :status
+      t.integer :author_id
+      t.integer :language_id
       t.timestamps
     end
   end
