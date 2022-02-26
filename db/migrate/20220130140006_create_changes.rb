d9db1b6192f0f7d9618d431c1441796be3d9a00b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-26 16:33:36 +0500

    author_id added to changes migration

--- db/migrate/20220130140006_create_changes.rb
+++ db/migrate/20220130140006_create_changes.rb
@@ -2,6 +2,7 @@ class CreateChanges < ActiveRecord::Migration[6.0]
   def change
     create_table :changes do |t|
       t.string :name
+      t.integer :author_id
       t.timestamps
     end
   end
