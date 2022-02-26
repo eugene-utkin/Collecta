c59b10114c8ab2ea40c2c16b579618a7efb61e18
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-26 16:34:29 +0500

    language_id added to changes migration

--- db/migrate/20220130140006_create_changes.rb
+++ db/migrate/20220130140006_create_changes.rb
@@ -3,6 +3,7 @@ class CreateChanges < ActiveRecord::Migration[6.0]
     create_table :changes do |t|
       t.string :name
       t.integer :author_id
+      t.integer :language_id
       t.timestamps
     end
   end
