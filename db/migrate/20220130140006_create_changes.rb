cdbc090d6b7e6c45165122b2c447634a15dc1e75
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-26 16:36:38 +0500

    description added to changes migration

--- db/migrate/20220130140006_create_changes.rb
+++ db/migrate/20220130140006_create_changes.rb
@@ -2,6 +2,7 @@ class CreateChanges < ActiveRecord::Migration[6.0]
   def change
     create_table :changes do |t|
       t.string :name
+      t.string :description
       t.integer :author_id
       t.integer :language_id
       t.timestamps
